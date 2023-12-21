7afab1872fb95929177334c371039fd499be423c
krokodalzik <nikitaopyhtin8356@gmail.com>
2023-12-21 23:46:13 +0300

    JavaUniversity2

---
+++ src/fourteen/IWaitlist.java
@@ -0,0 +1,11 @@
+package fourteen;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+interface IWaitList<E> {
+    void add(E element);
+    E remove();
+    boolean contains(E element);
+    boolean containsAll(Collection<E> c);
+    boolean isEmpty();
+}
---
+++ src/sixteen/Test.java
@@ -0,0 +1,8 @@
+package sixteen;
+
+public class Test {
+    public static void main(String[] args) {
+        ExceptionDemo demo = new ExceptionDemo();
+        demo.exceptionDemo();
+    }
+}
